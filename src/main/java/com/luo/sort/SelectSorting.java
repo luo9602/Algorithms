74826d3e291f28c3df9cf3b84159501e82520330
luo <750207960@qq.com>
2020-05-27 16:10:16 +0800

    SelectSorting

--- src/main/java/com/luo/sort/SelectSorting.java
+++ src/main/java/com/luo/sort/SelectSorting.java
@@ -10,10 +10,11 @@ public class SelectSorting {
     public static void sort(int[] arr) {
         int length = arr.length;
         int min, minIndex;
+        // 一共需要交换 length - 1 次
         for (int i = 0; i < length - 1; i++) {
             min = arr[i];
             minIndex = i;
-            // 遍历
+            // 遍历剩余的数据并与当前位置的数据对比
             for (int j = i + 1; j < length; j++) {
                 // 说明 min 不是真的最小值
                 if (min > arr[j]) {
